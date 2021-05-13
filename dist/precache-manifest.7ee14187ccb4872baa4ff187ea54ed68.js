86282a6a2ea94a7b56a55d704c725feeeefd197f
Oscar Garza <ogarza2881@gmail.com>
2021-05-12 21:24:01 -0500

    dist data

---
+++ dist/precache-manifest.7ee14187ccb4872baa4ff187ea54ed68.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad394c5758258ea40ac4",
+    "url": "/css/app.556410e2.css"
+  },
+  {
+    "revision": "ad394c5758258ea40ac4",
+    "url": "/js/app.e0a1e3c8.js"
+  },
+  {
+    "revision": "e2e2e2d75805588f474c",
+    "url": "/js/chunk-vendors.9ceaee78.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "083ca173ba52de14c3b9ec4861531aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "1f821ed0b3e56ad5e31c0d810f105ce1",
+    "url": "/img/app/fishing2.jpg"
+  },
+  {
+    "revision": "bb5c3ee1ea1303a335278d85ba20f862",
+    "url": "/img/app/lurus1.jpg"
+  },
+  {
+    "revision": "421052fee5650e5504847cf1d666069f",
+    "url": "/img/app/lurus2.jpg"
+  },
+  {
+    "revision": "30ab2366e9a24c9d2ec834014974ae67",
+    "url": "/img/app/logo.png"
+  },
+  {
+    "revision": "432355b026a3c828f04013c342eba79a",
+    "url": "/img/app/logo.jpg"
+  },
+  {
+    "revision": "7a8448fb5d86af66839f53d22838d124",
+    "url": "/img/app/lurus3.jpg"
+  },
+  {
+    "revision": "9204d7aa01a086cf4beccbc046fd264a",
+    "url": "/img/app/pic1.jpg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "ba3ba23610f8d38249d812a188df9ed6",
+    "url": "/img/app/pic2.jpg"
+  },
+  {
+    "revision": "c632479354a84fa7be88b85d774300d5",
+    "url": "/img/app/fishing1.jpg"
+  }
+];
